e6fdabdfc4de2bfa7b18bf20762c4418b6b73953
ltg <2440067458@qq.com>
2018-11-24 11:45:28 +0800

    Site updated: 2018-11-24 11:45:11

--- 2018/11/06/javascript/toki.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-function c(){
-	var time = new Date();
-	var t = time.toLocaleString();
-	document.getElementById("asd").innerHTML = t;
-	setTimeout(c,1000);
-}
-window.onload(c());
